7a4eec4375ae4cb9ca9251d520157172187b5074
Thomas <ThommyDude@users.noreply.github.com>
2018-05-31 18:27:02 +0200

    Cleaned Menu.js

--- client/components/menu.js
+++ client/components/menu.js
@@ -1,59 +1,21 @@
 const MenuComponent = {
 
   template: `
-   <ul class="nav">
-      <li class="nav-item">
-        <router-link class="nav-link" to="/">Hem</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/products">Produkter</router-link>
-      </li>
-      <li class="nav-item">
-      <router-link class="nav-link" to="/cart">Kundvagn</router-link>
-    </li>
-    <li class="nav-item">
-    <router-link class="nav-link" to="/login">Login/Regristrera</router-link>
-    </li>
-    <li class="nav-item">
-    <router-link class="nav-link" to="/admin">Admin</router-link>
-    </li>
-
+    <ul class="nav">
+        <li class="nav-item">
+            <router-link class="nav-link" to="/">Hem</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/products">Produkter</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/cart">Kundvagn</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login/Regristrera</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/admin">Admin</router-link>
+        </li>
     </ul>`
-
-
-    // <li class="nav-item">
-    // <input name="search" v-model="search"></input>
-    // <button v-onclick:search>Sök</button>
-
-    // </li>
-
-
-    // ,
-
-    // data(){
-    //   return{
-    //       search: ''
-    //   }
-    // },
-    
-    // methods: {
-      
-    //   search: () =>{
-    //       console.log('searc');
-    
-    //       http.post('/rest/search', {
-          
-    //         }).then(response => {
-    //           console.log(response);
-    
-    //         }).catch(error => {
-    //           console.error(error)
-    //         });
-    //       }
-    //     }    
-    
-  
 }
-
-
-
