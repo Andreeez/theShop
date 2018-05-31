dc4065a989a204a7b9d505d6d6fd5c8192f66cee
Thomas <ThommyDude@users.noreply.github.com>
2018-05-31 18:06:17 +0200

    Form looks a BIT better
    Still pretty bad :cry:

--- client/pages/cart-page.js
+++ client/pages/cart-page.js
@@ -5,17 +5,22 @@ const CartPageComponent = {
         <div class="col-12 card-body">
         <h1>Kundvagn</h1>
         <cart class="col-12"></cart>
-            <div class="col-12">
+            <div class="col-10">
                 <label for="first-name">Förnamn:</label>
                 <input name="first-name" v-model="firstName"></input>
+                <br/>
                 <label for="last-name">Efternamn:</label>
                 <input name="last-name" v-model="lastName"></input>
+                <br/>
                 <label for="adress">Adress:</label>
                 <input name="adress" v-model="adress"></input>
+                <br/>
                 <label for="phonenumber">Telefonnummer:</label>
                 <input name="first-name" v-model="phoneNumber"></input>
+                <br/>
                 <label for="customer-email">Email:</label>
                 <input name="customer-email" v-model="customerEmail"></input>
+                <br/>
                 <button v-on:click="pay">Betala</button>
             </div>
         </div>
